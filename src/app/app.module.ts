8f32ff41168a9d866e6bf9e516817aea5dbf513a
ltb-bossuyt <luongthibien.bossuyt@gmail.com>
2019-01-10 09:39:34 +0100

    Correctif header + add ng bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -57,6 +58,7 @@ import { HeaderComponent } from "./header/header.component";
     ],
     imports: [
         BrowserModule,
+        NgbModule.forRoot(),
         AppRoutingModule,
         BrowserAnimationsModule,
         CdkTableModule,
