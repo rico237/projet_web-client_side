47321954bed067df5a26ca19c3e12db886b3e3a3
ltb-bossuyt <luongthibien.bossuyt@gmail.com>
2019-01-17 11:17:18 +0100

    Add theme

---
+++ src/business/receipe.ts
@@ -0,0 +1,16 @@
+export class Receipe {
+    public _id: string;
+    public additives_original_tags: string;
+    public nutrition_score_debug: string;
+    public nutrition_data_prepared_per: string;
+    public _keywords: string[];
+    public ingredients_from_palm_oil_tags: string[];
+    public creator: string;
+    public lang: string;
+    public pnns_groups_2_tags: string[];
+    public ingredients_hierarchy: string[];
+    public lang_debug_tags: string[];
+    public ingredients_that_may_be_from_palm_oil_tags: string[];
+    public states_tags: string[];
+    public quantity: string[];
+}
