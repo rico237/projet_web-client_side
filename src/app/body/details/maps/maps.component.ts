a676685a20418b04d9c4cc6a5cd9abb3b89aae00
AlexGreau <alexgreau@live.fr>
2019-02-23 18:11:54 +0100

    multi marker & multi infos sur maps

--- src/app/body/details/maps/maps.component.ts
+++ src/app/body/details/maps/maps.component.ts
@@ -1,45 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodService } from "../../../../services/food.service";
+import { Store } from "../../../../business/store";
 
 @Component({
-  selector: 'app-maps',
-  templateUrl: './maps.component.html',
-  styleUrls: ['./maps.component.css']
+	selector: 'app-maps',
+	templateUrl: './maps.component.html',
+	styleUrls: ['./maps.component.css']
 })
 export class MapsComponent implements OnInit {
-  lat: number = 51.673858;
-  lng: number = 7.815982;
-  title: string = "Localisation"
+	lat: number = 51.673858;
+	lng: number = 7.815982;
+	title: string = "Localisation"
+	public dispos: Store[] = [];
+	constructor(private foodService: FoodService) { }
 
-  markers: Marker[] = [
-	  {
-		  lat: 51.673858,
-		  lng: 7.815982,
-		  label: 'A',
-		  draggable: true
-	  },
-	  {
-		  lat: 51.373858,
-		  lng: 7.215982,
-		  label: 'B',
-		  draggable: false
-	  },
-	  {
-		  lat: 51.723858,
-		  lng: 7.895982,
-		  label: 'C',
-		  draggable: true
-	  }
-  ]
-  constructor() { }
+	ngOnInit() {
+		this.getDispos();
 
-  ngOnInit() {
-  }
+	}
 
-}
-
-interface Marker {
-	lat: number;
-	lng: number;
-	label?: string;
-	draggable: boolean;
+	getDispos() {
+		this.foodService.getStores("5c658547d903a34c306ee7ea").subscribe((stor) => {
+			const prices = stor.prices;
+			this.dispos = [];
+			prices.forEach((value) => {
+				const store: Store = new Store();
+				store._id = value._id;
+				store.id_product = value.id_product;
+				store.id_store = value.id_store;
+				store.name_store = value.name_store;
+				store.adresse = value.adresse;
+				store.lat = value.lat;
+				store.long = value.long;
+				store.prix = value.prix;
+				this.dispos.push(store);
+			})
+			this.lat = parseFloat(this.dispos[0].lat);
+			this.lng = parseFloat(this.dispos[0].long);
+			console.log(this.lat);
+			console.log(this.lng);
+			console.log(this.dispos);
+		});
+	}
 }
